5d69c1f5ef67280db8c91ff9afdee0b84d179d4f
devruhul <ruhulamin262001@gmail.com>
2022-10-10 16:10:02 +0600

    fix: fix the style update some layout

--- navbar.js
+++ navbar.js
@@ -2,12 +2,14 @@ let openBtn = document.querySelector(".bi-list");
 let closeBtn = document.querySelector(".bi-x-lg");
 let menu = document.querySelector(".navbar_menu");
 
+// when menu icon show hide the close icon
 openBtn.addEventListener("click", () => {
     openBtn.classList.toggle("hide");
     closeBtn.classList.toggle("hide");
     menu.classList.toggle("hidden");
 });
 
+// when close icon show hide the menu icon
 closeBtn.addEventListener("click", () => {
     openBtn.classList.toggle("hide");
     closeBtn.classList.toggle("hide");
